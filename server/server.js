d084506770a4ace4225664a959d8eef6e958b5a0
jtuburan <jasontuburan@gmail.com>
2017-09-05 11:18:46 +0900

    const port value typho

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ var {Todo} = require('./models/todo.js');
 var {User} = require('./models/user.js');
 
 var app = express();
-const port = process.enc.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.post('/todos', (req,res) => {
